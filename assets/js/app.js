ba7ddee221f9f9e76712e3cf655c3b305cabe3b5
Mone <simone.fabiano@gmail.com>
2012-08-06 11:15:40 +0200

    change navigation code to avoid using ids as button's targets; use a custom data-target property to "configure" a button's target. introduce a placeholder for a realtime-data section. change startup message.

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,34 +5,35 @@ run(function () {
     // immediately invoked on first run
     var init = (function () {
         if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
+            alert("No internet connection - we won't be able to show you any maps or stocks");
         } else {
-            alert("We can reach Google - get ready for some awesome maps!");
+            alert("We can reach the Interwebz - get ready for some awesome maps and real-time goodness!");
         }
     })();
     
     // a little inline controller
-    when('#welcome');
-    when('#settings', function() {
-		// load settings from store and make sure we persist radio buttons.
-		store.get('config', function(saved) {
-			if (saved) {
-				if (saved.map) {
-					x$('input[value=' + saved.map + ']').attr('checked',true);
-				}
-				if (saved.zoom) {
-					x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
-				}
-			}
-		});
-	});
-    when('#map', function () {
+    when('welcome');
+    when('sld');
+    when('settings', function() {
+      // load settings from store and make sure we persist radio buttons.
+      store.get('config', function(saved) {
+        if (saved) {
+          if (saved.map) {
+            x$('input[value=' + saved.map + ']').attr('checked',true);
+          }
+          if (saved.zoom) {
+            x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
+          }
+        }
+      });
+    });
+    when('map', function () {
         store.get('config', function (saved) {
             // construct a gmap str
             var map  = saved ? saved.map || ui('map') : ui('map')
             ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
             ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-			
+      
             navigator.geolocation.getCurrentPosition(function (position) {
                 var location = "" + position.coords.latitude + "," + position.coords.longitude;
                 path += location + "&zoom=" + zoom;
@@ -45,7 +46,7 @@ run(function () {
             });
         });
     });
-    when('#save', function () {
+    when('save', function () {
         store.save({
             key:'config',
             map:ui('map'),
@@ -54,3 +55,4 @@ run(function () {
         display('#welcome');
     });
 });
+
